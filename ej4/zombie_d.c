56401e6c785ea6da15df0c48e737bd04261167dd
Nadissa <vel23764@uvg.edu.gt>
2026-03-01 23:30:52 -0600

    More time

--- ej4/zombie_d.c
+++ ej4/zombie_d.c
@@ -2,26 +2,32 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-int main() {
+int main()
+{
     pid_t hijo;
     hijo = fork();
-    if(hijo == 0) {
+    if (hijo == 0)
+    {
         // PROCESO HIJO
         printf("Hijo: Mi PID es %d, comenzando conteo...\n", getpid());
         // Conteo MUCHO más largo: 40 millones de iteraciones
-        for(int i = 1; i <= 40000000; i++) {
-            if(i % 100000 == 0) {  // Imprimir cada millón
-                printf("Hijo: %d / 40000000\n", i);
+        for (int i = 1; i <= 200000000; i++)
+        {
+            if (i % 100000 == 0)
+            {
+                printf("Hijo: %d / 200000000\n", i);
             }
         }
         printf("Hijo: Terminé el conteo, finalizando...\n");
         return 0;
     }
-    else {
+    else
+    {
         // PROCESO PADRE
         printf("Padre: Mi PID es %d, hijo PID %d\n", getpid(), hijo);
         printf("Padre: Entrando en ciclo infinito (sin wait)...\n");
-        while(1) {
+        while (1)
+        {
             sleep(1);
         }
     }
